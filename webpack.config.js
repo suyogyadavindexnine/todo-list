9f36eaefd52359c45cc315c4613634a083f27120
Suyog Yadav <suyog.yadav@indexnine.com>
2021-06-30 19:09:27 +0530

    Folder Name Changed

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,6 @@ const path = require('path');
   entry: './src/index.js',
    output: {
     filename: 'main.js',
-     path: path.resolve(__dirname, 'dist'),
+     path: path.resolve(__dirname, 'docs'),
    },
  };
